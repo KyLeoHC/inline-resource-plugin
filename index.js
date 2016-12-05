7ae2f26694d3fe2ce7ecddc9f0de27aa03c3c56d
apple <kyleo.hc@foxmail.com>
2016-12-05 17:32:06 +0800

    fix event method

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ InlineResourcePlugin.prototype.apply = function (compiler) {
         if (self.detectChange(compilation)) {
             //if content has been changed
             //just let other plugins know that we have already recompiled file
-            compiler.applyPluginsAsyncSeries(config.AFTER_EMIT_EVENT, function () {
+            compiler.applyPluginsAsyncWaterfall(config.AFTER_EMIT_EVENT, {}, function () {
             });
         }
         callback && callback();
